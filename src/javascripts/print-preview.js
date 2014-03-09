26bcd3326c4a2a6e1f3e5c6592b1d19d5e9379cd
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-03-09 17:43:05 +0900

    print preview

---
+++ src/javascripts/print-preview.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("body > .wrapper > .header").remove();
+  $("body > .wrapper > .site > .pagehead").remove();
+  $("body > .wrapper > .site > .container > div > .repository-sidebar").remove();
+  $('#js-repo-pjax-container > .tabnav').remove();
+  $('#head > .wiki-actions').remove();
+  $('#gollum-footer').remove();
+  $('body > .container').remove();
+});
